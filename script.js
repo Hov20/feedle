f72ca7c53e67cced49f29352c093ef44dd1a7c6e
Hovo Harutyunyan <HovHyan16@gmail.com>
2021-07-14 22:14:34 +0800

    feedle

---
+++ script.js
@@ -0,0 +1,32 @@
+// function bla(g) {
+//     let x = {};
+//     for(let i = 0; i < g.length; i++) {
+//         if(x[g[i]]) {
+//             x[g[i]]++;
+//         } else {x[g[i]] = 1}
+//     }
+//     return x;
+// }
+// console.log(bla(['a','b','c','c','a','b','a']));
+ 
+// const bilion = 1e6;
+// console.log(bilion);
+
+// let arr = ['Davo','Hovo','Ashot'];
+// let arrmas = arr.map(item => {
+//     return item.length;
+// });
+// console.log(arrmas);
+
+// const arrowf = (newarray) => {
+//     return newarray.filter(obj => {
+//         let allofsum = 0;
+//         obj.notes.map(el => allofsum += el);
+//         return allofsum >= 8;
+//     });
+// };
+// console.log(arrowf([
+//     { "name": "John", "notes": [3, 5, 4] },
+//     { "name": "Max", "notes": [1, 4, 6] },
+//     { "name": "Zygmund", "notes": [1, 2, 3] }
+// ]));
